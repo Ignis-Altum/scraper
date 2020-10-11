0ccb6c5e8e08b389dff027d0e89a01b28a6288a2
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2020-10-11 22:35:04 +0200

    Update from Crinibus/scraper

--- shell-scripts/update_scraper.sh
+++ shell-scripts/update_scraper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get update from upstream
+# Get update from upstream (changes in code)
 cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
@@ -8,5 +8,16 @@ git pull upstream master --commit --no-edit
 git add .
 git commit -m "Update from Crinibus/scraper"
 
-# Push to origin
+# Push updates from Crinibus/scraper to origin
+git push origin master
+
+
+# Get updates from origin (new links and stuff)
+git pull origin master --commit --no-edit
+
+# Commit changes
+git add .
+git commit -m "Update from Raspi"
+
+# Push updates to origin
 git push origin master
