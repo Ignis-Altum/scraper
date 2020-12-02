a3a039bba913a486e399cf953c88b8ece5e518da
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2020-12-02 23:41:00 +0100

    Comment out laptop link

--- tech_scraper/scrape_links.py
+++ tech_scraper/scrape_links.py
@@ -14,7 +14,7 @@ scraper.Elgiganten('baerbar', 'https://www.elgiganten.dk/product/gaming/gaming-l
 scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1159926/gaming/gaming-pc/baerbar/asus-tuf-gaming-a15-fx506iv-156-fhd-144-hz#')
 scraper.Proshop('baerbar', 'https://www.proshop.dk/Baerbar/ASUS-TUF-Gaming-A17-FX706IU-H7209T/2877154')
 scraper.Proshop('baerbar', 'https://www.proshop.dk/Baerbar/ASUS-ROG-Zephyrus-M15-GU502LU-AZ027T/2884762')
-scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1159657/gaming/gaming-pc/baerbar/lenovo-legion-5i-156-fhd-240-hz-500-nits')
+#scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1159657/gaming/gaming-pc/baerbar/lenovo-legion-5i-156-fhd-240-hz-500-nits')
 scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1159925/gaming/gaming-pc/baerbar/asus-tuf-gaming-a15-fx506ii-156-fhd-144-hz')
 scraper.Proshop('baerbar', 'https://www.proshop.dk/Baerbar/ASUS-ZenBook-Duo-UX481FL-HJ091R/2866060')
 scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1147203/gaming/gaming-pc/baerbar/komplett-khameleon-p9-baerbar-gaming-pc')
