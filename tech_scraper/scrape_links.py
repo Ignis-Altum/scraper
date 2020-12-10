e71d691b6f9044cb4729776790d28509d5a292f7
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2020-12-11 00:02:17 +0100

    Temporary comment-out the latest link

--- tech_scraper/scrape_links.py
+++ tech_scraper/scrape_links.py
@@ -30,4 +30,4 @@ scraper.Komplett('gpu', 'https://www.komplett.dk/product/1171385/hardware/pc-kom
 scraper.Komplett('baerbar', 'https://www.komplett.dk/product/1166278/gaming/gaming-pc/baerbar/asus-tuf-gaming-f15-fx506lu-156-fhd-144-hz')
 scraper.Computersalg('baerbar', 'https://www.computersalg.dk/i/6713520/asus-rog-strix-gaming-g512lv-az012t?sq=6713520')
 scraper.Proshop('baerbar', 'https://www.proshop.dk/Baerbar/ASUS-TUF-Gaming-A15-FX506IV-HN330T/2876644')
-scraper.Komplett('gpu', 'https://www.komplett.dk/product/1168755/hardware/pc-komponenter/grafikkort/asus-geforce-rtx-3070-rog-strix-oc')
+#scraper.Komplett('gpu', 'https://www.komplett.dk/product/1168755/hardware/pc-komponenter/grafikkort/asus-geforce-rtx-3070-rog-strix-oc')
