3ca76c00287956266570dbedaf6403a8e3af09bd
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2021-03-31 12:04:57 +0200

    Update shell-scripts/run_scraper.sh to only commit once per day

--- shell-scripts/run_scraper.sh
+++ shell-scripts/run_scraper.sh
@@ -4,21 +4,18 @@
 cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
-# Commit update
-#git add .
-#git commit -m "Update from Crinibus/scraper"
-
 # Run python-script
 cd /home/pi/Documents/scraper/tech_scraper/
 python3 scrape_links.py
 
-# Commit change in logfile.log
+# Commit change in logfile.log and records.json
 git add logfile.log
-git commit -m "Daily update logfile.log"
+git add records.json
+git commit -m "Daily update logs and records"
 
 # Commit change in records.json
-git add records.json
-git commit -m "Daily update records.json"
+# git add records.json
+# git commit -m "Daily update records.json"
 
 # Push to origin
 git push origin master
