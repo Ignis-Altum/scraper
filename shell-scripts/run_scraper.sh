ba859075808c89102d0f8b446b91b43054387a8f
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2021-04-09 21:59:02 +0200

    Update from Raspi

--- shell-scripts/run_scraper.sh
+++ shell-scripts/run_scraper.sh
@@ -5,12 +5,11 @@ cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
 # Run python-script
-cd /home/pi/Documents/scraper/tech_scraper/
-python3 scrape_links.py
+python3 main.py -s --threads
 
 # Commit change in logfile.log and records.json
-git add logfile.log
-git add records.json
+git add scraper/logfile.log
+git add scraper/records.json
 git commit -m "Daily update logs and records"
 
 # Commit change in records.json
