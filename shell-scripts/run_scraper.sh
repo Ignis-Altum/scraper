aef304bf50c7725fa914c7b95d853cc6564cecc5
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2021-04-10 12:23:12 +0200

    Dont scrape with threads for now

--- shell-scripts/run_scraper.sh
+++ shell-scripts/run_scraper.sh
@@ -5,7 +5,7 @@ cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
 # Run python-script
-python3 main.py -s --threads
+python3 main.py -s
 
 # Commit change in logfile.log and records.json
 git add scraper/logfile.log
