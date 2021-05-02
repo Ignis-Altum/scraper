902421b7477eb9d3213f17952fc818d725495ad5
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2021-05-02 15:44:53 +0200

    Run main.py with threads

--- shell-scripts/run_scraper.sh
+++ shell-scripts/run_scraper.sh
@@ -5,7 +5,7 @@ cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
 # Run python-scripts
-python3 main.py -s
+python3 main.py -s --threads
 python3 scrape_on_hold_products.py
 
 # Commit change in logfile.log and records.json
