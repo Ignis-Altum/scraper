b18fb25a1ac279049b7c4e56d461ffea4cdd6da9
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2021-05-02 15:44:27 +0200

    Also run scrape_on_hold_products.py

--- shell-scripts/run_scraper.sh
+++ shell-scripts/run_scraper.sh
@@ -4,17 +4,14 @@
 cd /home/pi/Documents/scraper/
 git pull upstream master --commit --no-edit
 
-# Run python-script
+# Run python-scripts
 python3 main.py -s
+python3 scrape_on_hold_products.py
 
 # Commit change in logfile.log and records.json
 git add scraper/logfile.log
 git add scraper/records.json
 git commit -m "Daily update logs and records"
-
-# Commit change in records.json
-# git add records.json
-# git commit -m "Daily update records.json"
 
 # Push to origin
 git push origin master
